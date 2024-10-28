9407767bc9b49a0ca51a0cf72d202c3afc5eefab
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-10-28 11:48:24 +0700

    updated

--- array_and_arraylist_in_java/src/com/in28minutes/array/Student.java
+++ array_and_arraylist_in_java/src/com/in28minutes/array/Student.java
@@ -1,5 +1,8 @@
 package com.in28minutes.array;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class Student {
 	private String name;
 	private int[] marks;
@@ -17,6 +20,10 @@ public class Student {
 		return this.marks;
 	}
 	
+	public int getNumberOfMarks() {
+		return this.marks.length;
+	}
+	
 	public int getTotalSumOfMarks() {
 		int sum = 0;
 		for(int i = 0; i <= this.marks.length - 1; i++) {
@@ -24,5 +31,37 @@ public class Student {
 		}
 		
 		return sum;
+	}
+	
+	public int getMaximumMark() {
+		int max = 0;
+		for(int i = 0; i <= this.marks.length - 1; i++) {
+			if(this.marks[i] >= max) {
+				max = this.marks[i];
+			}
+		}
+		
+		return max;
+	}
+	
+	public int getMinimumMark() {
+		int min = this.marks[0];
+		for(int i = 1; i <= this.marks.length - 1; i++) {
+			if(min >= this.marks[i]) {
+				min = this.marks[i];
+			}
+		}
+		
+		return min;
+	}
+	
+	public BigDecimal getAverageMark() {
+		int sum = this.getTotalSumOfMarks();
+		int count = this.getNumberOfMarks();
+		
+		BigDecimal result = new BigDecimal(sum)
+				.divide(new BigDecimal(count),3,RoundingMode.HALF_UP);
+		
+		return result;
 	}
 }
--- array_and_arraylist_in_java/src/com/in28minutes/array/StudentRunner.java
+++ array_and_arraylist_in_java/src/com/in28minutes/array/StudentRunner.java
@@ -5,6 +5,18 @@ public class StudentRunner {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Student student1 = new Student("Aaron", new int[] {
+				5,8,9
+		});
+		
+		System.out.println(student1.getNumberOfMarks());
+		System.out.println(student1.getTotalSumOfMarks());
+		
+		System.out.println(student1.getMaximumMark());
+		
+		System.out.println(student1.getMinimumMark());
+		
+		System.out.println(student1.getAverageMark());
 	}
 
 }
