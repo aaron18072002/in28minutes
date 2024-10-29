b08c7ce18825ca2c13441725d5d00e54a91841be
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-10-29 15:17:11 +0700

    updated

--- object-oriented-programming-level2/src/com/in28minutes/oop/lv2/Rectangle.java
+++ object-oriented-programming-level2/src/com/in28minutes/oop/lv2/Rectangle.java
@@ -4,10 +4,32 @@ public class Rectangle {
 	private float length;
 	private float width;
 	
+	public float getLength() {
+		return length;
+	}
+
+	public void setLength(float length) {
+		if(length > this.width) {			
+			this.length = length;
+		}
+	}
+
+	public float getWidth() {
+		return width;
+	}
+
+	public void setWidth(float width) {
+		if(width < this.length) {		
+			this.width = width;
+		}
+	}
+
 	public Rectangle(float length, float width) {
 		if(length > 0 && width > 0 && length > width) {		
 			this.length = length;
 			this.width = width;
+		} else {
+			throw new IllegalArgumentException();
 		}
 	}
 	
@@ -17,5 +39,10 @@ public class Rectangle {
 	
 	public float perimeter() {
 		return (this.length+this.width)*2;
+	}
+	
+	public String toString() {
+		return String.format("length - %f, width -%f",
+				this.length, this.width);
 	}
 }
--- object-oriented-programming-level2/src/com/in28minutes/oop/lv2/RectangleRunner.java
+++ object-oriented-programming-level2/src/com/in28minutes/oop/lv2/RectangleRunner.java
@@ -6,6 +6,14 @@ public class RectangleRunner {
 		// TODO Auto-generated method stub
 		Rectangle rectangle1 = new Rectangle(5.55f,3.33f);
 		
+		System.out.println(rectangle1.toString());
+		System.out.println(rectangle1.area());
+		System.out.println(rectangle1.perimeter());
+		
+		rectangle1.setLength(6.66f);
+		rectangle1.setWidth(2.22f);
+		
+		System.out.println(rectangle1.toString());
 		System.out.println(rectangle1.area());
 		System.out.println(rectangle1.perimeter());
 	}
