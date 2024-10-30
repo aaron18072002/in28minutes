f23bdcf269568e774bb38edb7501a0412081d526
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-10-30 09:23:15 +0700

    updated

---
+++ object-oriented-programming-level2/src/com/in28minutes/oop/inheritance/Person.java
@@ -0,0 +1,30 @@
+package com.in28minutes.oop.inheritance;
+
+public class Person {
+	private String id;
+	private String name;
+	private String email;
+	
+	public Person() {
+		
+	}
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
---
+++ object-oriented-programming-level2/src/com/in28minutes/oop/inheritance/PersonRunner.java
@@ -0,0 +1,14 @@
+package com.in28minutes.oop.inheritance;
+
+public class PersonRunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student student1 = new Student();
+		
+		student1.setId("1");
+		student1.setEmail("in28minutes@gmai.com");
+		student1.setName("Ranga");
+	}
+
+}
