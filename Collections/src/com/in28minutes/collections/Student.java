ef3c16de62d8b660c5cd49d54fcd1a7eacad1e81
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-10-31 18:15:05 +0700

    updated

--- Collections/src/com/in28minutes/collections/Student.java
+++ Collections/src/com/in28minutes/collections/Student.java
@@ -1,9 +1,9 @@
 package com.in28minutes.collections;
 
-public class Student {
+public class Student implements Comparable<Student> {
 	private int id;
 	private String name;
-	
+
 	public Student(int id, String name) {
 		this.id = id;
 		this.name = name;
@@ -16,10 +16,14 @@ public class Student {
 	public String getName() {
 		return name;
 	}
-	
+
 	@Override
 	public String toString() {
-		return String.format("id - %d,name - %s",
-				this.id, this.name);
+		return String.format("id - %d,name - %s", this.id, this.name);
+	}
+
+	@Override
+	public int compareTo(Student student) {
+		return Integer.compare(this.id, student.id);
 	}
 }
--- Collections/src/com/in28minutes/collections/StudentsCollectionRunner.java
+++ Collections/src/com/in28minutes/collections/StudentsCollectionRunner.java
@@ -1,15 +1,31 @@
 package com.in28minutes.collections;
 
 import java.util.ArrayList;
+//import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class StudentsCollectionRunner {
-
+	
+	public static class DescendingStudentId implements Comparator<Student> {
+		@Override
+		public int compare(Student student1, Student student2) {
+			// TODO Auto-generated method stub
+			return Integer.compare(student2.getId(), student1.getId());
+		}	
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		List<Student> students = new ArrayList<Student>();
 		
-		students.add(new Student(1, "Ranga"));
+		students.add(new Student(3, "Eve"));
 		students.add(new Student(2, "Adam"));
+		students.add(new Student(1, "Ranga"));
+		
+//		Collections.sort(students);
+		students.sort(new DescendingStudentId());
+		
+		System.out.println(students.toString());
 	}
 }
