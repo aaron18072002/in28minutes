bc99794aada800a737c6ca3e0408b53ae481c755
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-11-04 09:49:14 +0700

    updated

--- functional_programming/src/com/in28minutes/functional_programming/FunctionalProgrammingRunner.java
+++ functional_programming/src/com/in28minutes/functional_programming/FunctionalProgrammingRunner.java
@@ -6,20 +6,28 @@ public class FunctionalProgrammingRunner {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		List<String> list = List.of("Banana","Apple","Dolphin","Shark");
+		List<String> list = List.of("Banana","Apple","Dolphin","Shark","Bat","Cat");
 		
 		printBasic(list);
 		System.out.println("------------");
-		printWithStream(list);
+//		printWithStream(list);
+		printWithStreamAndFilter(list);
 	}
 	
 	private static void printBasic(List<String> list) {
 		for(int i = 0; i <= list.size() - 1; i++) {
-			System.out.println(list.get(i));
+			if(list.get(i).toLowerCase().endsWith("at")) {			
+				System.out.println(list.get(i));
+			}
 		}
 	}
 	
 	private static void printWithStream(List<String> list) {
 		list.stream().forEach(str -> System.out.println(str));
+	}
+	
+	private static void printWithStreamAndFilter(List<String> list) {
+		list.stream().filter(str -> str.toLowerCase().endsWith("at"))
+					 .forEach(str -> System.out.println(str));
 	}
 }
