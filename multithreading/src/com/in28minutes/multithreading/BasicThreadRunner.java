7adf3e64e2d21e4c6cf69374b0444717bd902982
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-11-05 18:12:22 +0700

    updated

--- multithreading/src/com/in28minutes/multithreading/BasicThreadRunner.java
+++ multithreading/src/com/in28minutes/multithreading/BasicThreadRunner.java
@@ -3,11 +3,24 @@ package com.in28minutes.multithreading;
 public class BasicThreadRunner {
 	public static class Task1 extends Thread {
 		public void run() {
-			System.out.println("Task1 started\n");
+			System.out.println("Task1 started");
 			for (int i = 100; i <= 199; i++) {
 				System.out.print(i + "\t");
 			}
+			System.out.println("\nTask1 done:");
 		}
+	}
+	
+	public static class Task2 implements Runnable {
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			System.out.println("Task2 started");
+			for (int i = 200; i <= 299; i++) {
+				System.out.print(i + "\t");
+			}
+			System.out.println("\nTask2 done:");
+		}		
 	}
 	
 	public static void main(String[] args) {
@@ -17,21 +30,18 @@ public class BasicThreadRunner {
 		System.out.println("Task1 kick off");
 		Thread task1 = new Task1();
 		task1.start();
-		System.out.println("Task1 done: \n");
 		
 		//Task2
 		System.out.println("Task2 kick off");
-		for (int i = 200; i <= 299; i++) {
-			System.out.print(i + "\t");
-		}
-		System.out.println("Task2 done: \n");
+		Thread threadOfTask2 = new Thread(new Task2());
+		threadOfTask2.start();
 		
 		//Task3
 		System.out.println("Task3 kick off");
 		for (int i = 300; i <= 399; i++) {
 			System.out.print(i + "\t");
 		}
-		System.out.println("Task3 done");
+		System.out.println("\nTask3 done");
 		System.out.println("Main done");
 	}
 }
