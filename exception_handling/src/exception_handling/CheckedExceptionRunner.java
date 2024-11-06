6d7490e6cf5d7a602f794e490efc801bce30afa4
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-11-06 15:37:11 +0700

    updated

---
+++ exception_handling/src/exception_handling/CheckedExceptionRunner.java
@@ -0,0 +1,16 @@
+package exception_handling;
+
+public class CheckedExceptionRunner {
+	public static void main(String[] args) {
+		method1();
+		method2();
+	}
+	
+	private static void method1() throws RuntimeException {
+		
+	}
+	
+	private static void method2() throws InterruptedException {
+		
+	}
+}
