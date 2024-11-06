e209ca72c3b5991f7c0338e6242243e44e278719
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-11-06 09:56:26 +0700

    updated

--- multithreading/src/com/in28minutes/multithreading/ExecutorServiceRunner.java
+++ multithreading/src/com/in28minutes/multithreading/ExecutorServiceRunner.java
@@ -3,14 +3,35 @@ package com.in28minutes.multithreading;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+class Task implements Runnable {
+	private final int number;
+	
+	public Task(int number) {
+		this.number = number;
+	}
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		System.out.printf("\nTask %d started", this.number).println();
+		for(int i = this.number*100; i <= this.number*100 + 99; i++) {
+			System.out.print(i + "\t");
+		}
+		System.out.printf("\nTask %d done", this.number).println();
+	}
+}
+
 public class ExecutorServiceRunner {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ExecutorService executorService = 
-				Executors.newSingleThreadExecutor();
+				Executors.newFixedThreadPool(5);
 		
-		executorService.execute(new BasicThreadRunner.Task1());
-		executorService.execute(new Thread(new BasicThreadRunner.Task2()));
+		executorService.execute(new Thread(new Task(1)));
+		executorService.execute(new Thread(new Task(2)));
+		executorService.execute(new Thread(new Task(3)));
+		executorService.execute(new Thread(new Task(4)));
+		executorService.execute(new Thread(new Task(5)));
 		
 		executorService.shutdown();
 	}
