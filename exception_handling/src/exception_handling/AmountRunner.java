631903556cedb68ef0854210a2cc40da504e513b
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-11-07 15:02:16 +0700

    updated

--- exception_handling/src/exception_handling/AmountRunner.java
+++ exception_handling/src/exception_handling/AmountRunner.java
@@ -2,16 +2,17 @@ package exception_handling;
 
 class Amount {
 	private String currency;
-	private int amount;
+	private int amount;	
 	
 	public Amount(String currency, int amount) {
-		this.currency = currency;
+		this.currency = currency.toUpperCase();
 		this.amount = amount;
 	}
 	
-	public void add(Amount amount) {
+	public void add(Amount amount) throws Exception {
 		if(!amount.currency.equals(this.currency)) {
-			throw new RuntimeException("Currency isnt match");
+			throw new Exception
+				("Currencies arent match " + this.currency + " and " + amount.currency);
 		}
 		this.amount += amount.amount;
 	}
@@ -23,7 +24,7 @@ class Amount {
 }
 
 public class AmountRunner {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		Amount usdAmount = new Amount("USD",50);
 		Amount euroAmount = new Amount("EUR",30);
 		usdAmount.add(euroAmount);
