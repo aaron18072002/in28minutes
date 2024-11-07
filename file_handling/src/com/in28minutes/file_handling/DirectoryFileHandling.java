16a4aafdef19741095dbcb095164f84b423bad8d
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-11-07 17:27:29 +0700

    updated

--- file_handling/src/com/in28minutes/file_handling/DirectoryFileHandling.java
+++ file_handling/src/com/in28minutes/file_handling/DirectoryFileHandling.java
@@ -4,11 +4,19 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.function.Predicate;
 
 public class DirectoryFileHandling {
 	public static void main(String[] args) throws IOException {
 		Path rootPath = Paths.get(".");
 		Files.list(rootPath)
-			.forEach(file -> System.out.println(file.getFileName()));
+			.forEach(path -> System.out.println(path));
+		
+		System.out.println("--------------");
+		
+		Predicate<Path> predicate = path -> 
+				path.toString().endsWith(".java");
+		Files.walk(rootPath, 5).filter(predicate)
+				.forEach(path -> System.out.println(path.toString()));
 	}
 }
