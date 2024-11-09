dd1939e01d90c4758972c442b2c1eff327b186bc
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-11-09 17:47:17 +0700

    updated

---
+++ java_tips/src/com/in28minutes/java_tips/ImportRunner.java
@@ -0,0 +1,13 @@
+package com.in28minutes.java_tips;
+
+import static java.lang.System.out;
+import static java.util.Collections.sort;
+
+public class ImportRunner {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		out.println("ABC");
+		
+		sort(null);
+	}
+}
