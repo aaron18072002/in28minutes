17d61b2cdc37b82d45d67c690373c1ca93e79692
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-11-09 18:24:06 +0700

    updated

---
+++ java_tips/src/com/in28minutes/java_tips/EqualsRunner.java
@@ -0,0 +1,46 @@
+package com.in28minutes.java_tips;
+
+class Client {
+	private int id;
+	
+	public Client(int id) {
+		super();
+		this.id = id;
+	}
+	
+	public int getId() {
+		return this.id;
+	}
+
+	@Override
+	public int hashCode() {
+		// TODO Auto-generated method stub
+		return super.hashCode();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		// TODO Auto-generated method stub
+		if(obj == null) {
+			return false;
+		}
+		Client comparedClient = (Client)obj;
+		if(this.id == comparedClient.getId()) {
+			return true;
+		}
+		
+		return false;
+	}
+}
+
+public class EqualsRunner {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Client client1 = new Client(1);
+		Client client2 = new Client(1);
+		Client client3 = new Client(3);
+		
+		System.out.println(client1.equals(client2));
+		System.out.println(client1.equals(client3));
+	}
+}
